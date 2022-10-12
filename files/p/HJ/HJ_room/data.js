59fa6d312d0d68c00fd515c1e1a9732602b46065
hwanjuny <fshwanjun@gmail.com>
2022-10-13 08:53:58 +0900

    start

---
+++ files/p/HJ/HJ_room/data.js
@@ -0,0 +1,209 @@
+var APP_DATA = {
+  "scenes": [
+      
+    {
+      "id": "0-main",
+      "name": "Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.013709653766746044,
+        "pitch": 0.029123182576125117,
+        "fov": 1.4015213061224467
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.711990761360827,
+          "pitch": -0.0010319068572997026,
+          "rotation": 12.566370614359176,
+          "target": "1-poster"
+        },
+        {
+          "yaw": -0.6125374560701964,
+          "pitch": 0.7915247008799486,
+          "rotation": 3.141592653589793,
+          "target": "3-chess"
+        },
+        {
+          "yaw": 1.5787845401237064,
+          "pitch": 0.21979745252184735,
+          "rotation": 0,
+          "target": "2-computer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1952017629756746,
+          "pitch": 0.4635147883224917,
+          "title": "<a style=\"font-size: 20px;\">Cororo Table</a>",
+          "text": "<a class=\"a_text\" href=\"page/Cororo_table/Cororo_table.html\" style=\"font-size: 18px;\">자세히 본다</a>"
+        },
+        {
+          "yaw": 2.539265366427662,
+          "pitch": 0.11527303973230474,
+          "title": "<a style=\"font-size: 20px;\">Door</a>",
+          "text": "<a class=\"a_text\" href=\"../../../All/cororohills_v1.html\" style=\"font-size: 18px;\">밖으로 나간다.</a>"
+        }
+      ]
+    },
+      
+    {
+      "id": "1-poster",
+      "name": "Poster",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.68542293506533,
+          "pitch": -0.4120890655166285,
+          "rotation": 0,
+          "target": "0-main"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3682908078043141,
+          "pitch": -0.1022066691298722,
+          "title": "<a style=\"font-size: 20px;\">Poster_1</a>",
+          "text": "<a class=\"a_text\" href=\"page/Poster_page/Poster_1.html\" style=\"font-size: 18px;\">자세히 본다</a>"
+        },
+        {
+          "yaw": 0.30025486338870877,
+          "pitch": -0.03855872714487418,
+          "title": "<a style=\"font-size: 20px;\">Poster_2</a>",
+          "text": "<a class=\"a_text\" href=\"page/Poster_page/Poster_2.html\" style=\"font-size: 18px;\">자세히 본다</a>"
+        },
+        {
+          "yaw": 0.7823445995341949,
+          "pitch": -0.07465188936388856,
+          "title": "<a style=\"font-size: 20px;\">Poster_3</a>",
+          "text": "<a class=\"a_text\" href=\"page/Poster_page/Poster_3.html\" style=\"font-size: 18px;\">자세히 본다</a>"
+        },
+        {
+          "yaw": -1.9624782573766666,
+          "pitch": 0.06649650302230725,
+          "title": "<a style=\"font-size: 20px;\">Poster_4</a>",
+          "text": "<a class=\"a_text\" href=\"page/Poster_page/Poster_4.html\" style=\"font-size: 18px;\">자세히 본다</a>"
+        }
+      ]
+    },
+      
+    {
+      "id": "2-computer",
+      "name": "Computer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.124044312172595,
+          "pitch": -0.45870446676087795,
+          "rotation": 0,
+          "target": "0-main"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.01570385112900574,
+          "pitch": 0.10504760000290503,
+          "title": "<a style=\"font-size: 20px;\">HJ Desktop</a>",
+          "text": "<a class=\"a_text\" href=\"page/Desktop/Desktop.html\" style=\"font-size: 18px;\">들어간다</a>"
+        }
+      ]
+    },
+      
+    {
+      "id": "3-chess",
+      "name": "Chess",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.50845929893337,
+          "pitch": -1.1649147005324814,
+          "rotation": 0,
+          "target": "0-main"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+    
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ files/p/HS/js/khs_room.js
@@ -0,0 +1,99 @@
+$(function () {
+    $('#fst_x5F_bg').click(function () {
+        $(this).css('display', 'none');
+    });
+});
+
+$(function () {
+    $('#i_x5F_1').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_1').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_1').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_2').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_2').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_2').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_3').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_3').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_3').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_4').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_4').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_4').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_5').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_5').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_5').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_6').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_6').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_6').css('opacity', '1.0');
+    });
+
+    $('#i_x5F_7').click(function () {
+        $('.bg_all').css('display', 'none');
+        $('#bg_x5F_7').css('display', 'block');
+        $('.icon_all').css('opacity', '0.3');
+        $('#i_x5F_7').css('opacity', '1.0');
+    });
+});
+
+$(function () {
+    $('#text_x5F_trigger').click(function () {
+        $('#text').toggle('slow');
+    });
+});
+
+$(function () {
+    $('#i_x5F_1').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_1').css('display', 'block');
+    });
+    
+    $('#i_x5F_2').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_2').css('display', 'block');
+    });
+    
+    $('#i_x5F_3').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_3').css('display', 'block');
+    });
+    
+    $('#i_x5F_4').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_4').css('display', 'block');
+    });
+    
+    $('#i_x5F_5').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_5').css('display', 'block');
+    });
+    
+    $('#i_x5F_6').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_6').css('display', 'block');
+    });
+    
+    $('#i_x5F_7').click(function () {
+        $('.text_all').css('display', 'none');
+        $('#t_x5F_7').css('display', 'block');
+    });
+});
